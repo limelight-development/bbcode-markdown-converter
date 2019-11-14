cf46815c28319414a69a93338ce011f208353ea5
John Internet <jonjon1234.github@gmail.com>
2019-11-14 11:51:25 +0000

    deleted the abstract converter.

--- src/AbstractConverter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-namespace Limelight\Converter;
-
-
-class AbstractConverter {
-
-}
---
+++ test_script.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+$conv = new \Limelight\Converter\Converter();
+$conv->setText(file_get_contents(__DIR__ . '/data/test_post.bb'));
+$conv->BBToMarkdown();
+file_put_contents(__DIR__ . '/data/test_post.md', $conv->getText());
