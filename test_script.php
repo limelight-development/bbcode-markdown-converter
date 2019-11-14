0f48945881dc2e4297ff700af14e41439600e53a
John Internet <jonjon1234.github@gmail.com>
2019-11-14 12:18:24 +0000

    use the chaining for the test script.

--- test_script.php
+++ test_script.php
@@ -1,7 +1,12 @@
 <?php
 
+use Limelight\Converter\Converter;
+
 require __DIR__ . '/vendor/autoload.php';
-$conv = new \Limelight\Converter\Converter();
-$conv->setText(file_get_contents(__DIR__ . '/data/test_post.bb'));
-$conv->bbToMarkdown();
-file_put_contents(__DIR__ . '/data/test_post.md', $conv->getText());
+
+file_put_contents(
+	__DIR__ . '/data/test_post.md',
+	(new Converter(file_get_contents(__DIR__ . '/data/test_post.bb')))
+		->bbToMarkdown()
+		->getText()
+);
