37b4f76a5e679de8bb60ba53c30e33941c50b1a5
John Internet <jonjon1234.github@gmail.com>
2019-11-24 15:42:39 +0000

    larger range of test scripts.

---
+++ test.php
@@ -0,0 +1,36 @@
+<?php
+
+use Limelight\Converter\Converter;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$bbcode = file_get_contents(__DIR__ . '/data/bbcode-source');
+
+$converter = new Converter();
+$converter->setText($bbcode);
+
+$converter->bbToMarkdown();
+file_put_contents(__DIR__ . '/data/bbcode-converted', $converter->getText());
+
+$converter->markdownToBB();
+file_put_contents(__DIR__ . '/data/bbcode-reversed', $converter->getText());
+
+$converter->setText($bbcode);
+$converter->bbToMarkdown()->bbToMarkdown();
+file_put_contents(__DIR__ . '/data/bbcode-double', $converter->getText());
+
+
+$markdown = file_get_contents(__DIR__ . '/data/markdown-source');
+
+$converter = new Converter();
+$converter->setText($markdown);
+
+$converter->markdownToBB();
+file_put_contents(__DIR__ . '/data/markdown-converted', $converter->getText());
+
+$converter->bbToMarkdown();
+file_put_contents(__DIR__ . '/data/markdown-reversed', $converter->getText());
+
+$converter->setText($markdown);
+$converter->markdownToBB()->markdownToBB();
+file_put_contents(__DIR__ . '/data/markdown-double', $converter->getText());
