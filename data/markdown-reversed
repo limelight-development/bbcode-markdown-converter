803da9bd405a7fc0a310a477f9a008b8b8696172
John Internet <jonjon1234.github@gmail.com>
2019-11-24 16:17:39 +0000

    New test script and outputs.

---
+++ data/markdown-reversed
@@ -0,0 +1,16 @@
+*this is italics*
+
+**this is bold**
+
+***this is both***
+
+**this is bold with *italics* inside**
+
+*this is italics with **bold** inside*
+
+[https://google.com](https://google.com)
+[limelight](https://limelightgaming.net)
+
+# this is a header
+
+## this is a l2 header
--- test.php
+++ test.php
@@ -10,27 +10,27 @@ $converter = new Converter();
 $converter->setText($bbcode);
 
 $converter->bbToMarkdown();
-file_put_contents(__DIR__ . '/data/bbcode-converted', $converter->getText());
-
-$converter->markdownToBB();
-file_put_contents(__DIR__ . '/data/bbcode-reversed', $converter->getText());
-
-$converter->setText($bbcode);
-$converter->bbToMarkdown()->bbToMarkdown();
-file_put_contents(__DIR__ . '/data/bbcode-double', $converter->getText());
-
-
-$markdown = file_get_contents(__DIR__ . '/data/markdown-source');
-
-$converter = new Converter();
-$converter->setText($markdown);
-
-$converter->markdownToBB();
-file_put_contents(__DIR__ . '/data/markdown-converted', $converter->getText());
-
-$converter->bbToMarkdown();
-file_put_contents(__DIR__ . '/data/markdown-reversed', $converter->getText());
-
-$converter->setText($markdown);
-$converter->markdownToBB()->markdownToBB();
-file_put_contents(__DIR__ . '/data/markdown-double', $converter->getText());
+//file_put_contents(__DIR__ . '/data/bbcode-converted', $converter->getText());
+//
+//$converter->markdownToBB();
+//file_put_contents(__DIR__ . '/data/bbcode-reversed', $converter->getText());
+//
+//$converter->setText($bbcode);
+//$converter->bbToMarkdown()->bbToMarkdown();
+//file_put_contents(__DIR__ . '/data/bbcode-double', $converter->getText());
+//
+//
+//$markdown = file_get_contents(__DIR__ . '/data/markdown-source');
+//
+//$converter = new Converter();
+//$converter->setText($markdown);
+//
+//$converter->markdownToBB();
+//file_put_contents(__DIR__ . '/data/markdown-converted', $converter->getText());
+//
+//$converter->bbToMarkdown();
+//file_put_contents(__DIR__ . '/data/markdown-reversed', $converter->getText());
+//
+//$converter->setText($markdown);
+//$converter->markdownToBB()->markdownToBB();
+//file_put_contents(__DIR__ . '/data/markdown-double', $converter->getText());
